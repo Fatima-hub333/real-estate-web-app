3841b47cdea13987802bb137ba4b1fcbc3292e5f
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-21 19:40:17 +0500

    Update files

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Box, Flex, Text, Avatar} from '@chakra-ui/layout';
+import { Box, Flex, Text} from '@chakra-ui/layout';
+import { Avatar } from '@chakra-ui/avatar';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
